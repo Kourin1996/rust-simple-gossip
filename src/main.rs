0ca03e9f54e3247f7d3e0c78ce917915742e3092
kourin <kourin.code@gmail.com>
2024-05-25 06:32:27 +0300

    feat: add logger setting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::Parser;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{fmt, Layer};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -26,4 +29,21 @@ fn main() {
 
     let args = Args::parse();
     println!("{:?}", args);
+
+    let env_filter = tracing_subscriber::EnvFilter::new(if args.debug { "debug" } else { "info" });
+    let stdout_formatter = fmt::format().compact();
+
+    let stdout_log = tracing_subscriber::fmt::layer()
+        .event_format(stdout_formatter)
+        .with_filter(env_filter);
+
+    tracing_subscriber::registry().with(stdout_log).init();
+
+    print_log();
+}
+
+fn print_log() {
+    tracing::debug!("Debugging enabled 1: {}", true);
+
+    tracing::info!("Debugging enabled 2: {}", false);
 }
